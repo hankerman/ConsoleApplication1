4c4d1415651a664b7976d16126ac51b362b38861
hankerman <stalker-2204@mail.ru>
2022-01-12 21:42:54 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+using namespace std;
+
+
+
+
+
+bool isPalindrome(int n) {
+    
+    bool result;
+    int a, revers = 0, c;
+    a = n;
+    while (a != 0) {
+        c = a % 10;
+        revers = revers * 10 + c;
+        a /= 10;
+    }
+    result = n == revers;
+    return result;
+}
+
+
+int main()
+{
+
+    int n;
+    cin >> n;
+
+    if (isPalindrome(n)) {
+        cout << n << " is a palindrome";
+    }
+    else {
+        cout << n << " is NOT a palindrome";
+    }
+    return 0;
+
+}
